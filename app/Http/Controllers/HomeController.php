bb7223028e1a7a424cd5d20d01f01353d1846072
Administrator <vo@shevchenko.website>
2019-12-31 18:55:27 +0300

    Minor tech updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\World;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 
 class HomeController extends Controller {
 
@@ -19,6 +21,11 @@ class HomeController extends Controller {
 	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
     public function index(Request $request) {
+
+        //Cache::remember('laravel:categories', 30, function() {return 'redis';});
+        //$data = Redis::set('laravel:categories', 'redis');
+        //$data = Redis::get('laravel:categories');
+        //dd($data);
 
 		$world = new World();
 
